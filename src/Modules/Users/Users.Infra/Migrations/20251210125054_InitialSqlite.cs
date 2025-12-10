c28be0f3388e114f5417b983aa331cf0a0ff7b05
Caio Chiabai <caiochiabaioliveira@gmail.com>
2025-12-10 10:25:28 -0300

    feat: configurado banco SQLite e criado migrations

---
+++ src/Modules/Users/Users.Infra/Migrations/20251210125054_InitialSqlite.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Users.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSqlite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "users_module");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "users_module",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "users_module");
+        }
+    }
+}
